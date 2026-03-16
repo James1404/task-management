5d7f844b7440ce12cc2b0fb18056ab4556177752
James Barnfather <jamesbarnfather99@gmail.com>
2026-03-16 15:49:47 +0000

    Switched endpoint to delete method

--- backend/src/routes/v1/account.ts
+++ backend/src/routes/v1/account.ts
@@ -17,8 +17,8 @@ export default async function routes(
 ) {
     await fastify.register(authPlugin);
 
-    fastify.post<{ Body: DeleteUserSchemaType }>(
-        "/delete",
+    fastify.delete<{ Body: DeleteUserSchemaType }>(
+        "/",
         {
             schema: {
                 body: DeleteUserSchema,
